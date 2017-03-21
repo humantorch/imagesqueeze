951c8f73d2785b867358d1f338f830b74a14b742
scott kosman <kosman.scott@gmail.com>
2017-03-21 12:05:48 +0100

    switched to a less-dumb npm package for image opt

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,84 +17,28 @@ module.exports = function(grunt) {
 
     };
 
-    var nconf = require('nconf');
-    nconf.argv().env();
+    var opt = require('imagemin-optipng');
 
-    //# Override flex sdk location
-    if ( nconf.get('FLEX') ) {
-        nconf.overrides({'config' : {'flex_sdk' : nconf.get('FLEX')}});
+grunt.initConfig({
+    imagemin: {
+        dynamic: {
+            options: {
+                optimizationLevel: 3,
+                svgoPlugins: [{ removeViewBox: false }],
+                use: [opt()]
+            },
+            files: [{
+                expand: true,
+                cwd: 'input/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'output/'
+            }]
+        }
     }
+});
 
-
-    //# Load grunt task configs
-    var taskConfigs = grunt.file.expand('tasks/enabled/**/*.config.js');
-
-    var config, configModule;
-    grunt.util._.each(taskConfigs,function( configPath ) {
-
-        configModule = require('./'+configPath);
-
-        config = configModule(grunt);
-
-        grunt.util._.extend(gruntConfig, config);
-    });
-
-    //# Set config defaults
-    nconf.defaults({ "config" : gruntConfig });
-
-    //# Set config on grunt
-    grunt.initConfig(nconf.get('config'));
-
-    //# Load grunt tasks
-    var tasks = grunt.file.expand(['tasks/enabled/**/*.js','!tasks/enabled/**/*.config.js']);
-
-    var taskModule;
-    grunt.util._.each(tasks,function( taskPath ) {
-
-        console.log(taskPath);
-        taskModule = require('./'+taskPath);
-        taskModule(grunt);
-
-    });
-
-    grunt.registerTask('update_deps', 'Add all enabled task dependencies to main package.json', function() {
-
-        var _ = grunt.util._;
-
-        var mainPackageJSON = grunt.file.readJSON("package.json");
-
-        grunt.log.debug(mainPackageJSON);
-
-        var taskPackages = grunt.file.expand(["tasks/enabled/**/package.json"]);
-
-        var depsJSON = taskPackages.map(function( packagePath ){
-            return grunt.file.readJSON( packagePath );
-        });
-
-        var depsCombined = depsJSON.reduce(function( deps, packDep ) {
-
-            if( packDep.hasOwnProperty('dependencies') ) {
-                _.defaults(deps.dependencies, packDep.dependencies)
-            }
-
-            if( packDep.hasOwnProperty('devDependencies') ) {
-                _.defaults(deps.devDependencies, packDep.devDependencies)
-            }
-
-            return deps;
-        },{
-            "dependencies": _.clone(mainPackageJSON.dependencies),
-            "devDependencies": _.clone(mainPackageJSON.devDependencies)
-        });
-
-        grunt.log.debug(taskPackages);
-
-        _.extend(mainPackageJSON,depsCombined);
-
-        grunt.file.write("package.json",JSON.stringify(mainPackageJSON, null, "\t"));
-    });
 
   // Default task.
-  grunt.registerTask('imagesqueeze', ['optimize-img-local']);
+  grunt.registerTask('imagesqueeze', ['imagemin']);
 
 };
